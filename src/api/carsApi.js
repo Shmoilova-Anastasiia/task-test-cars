18b3bab825b65bd211fb06d4220a1584d072f9cf
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-09-17 22:05:44 +0300

    delete coment

--- src/api/carsApi.js
+++ src/api/carsApi.js
@@ -3,9 +3,3 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://648cb0ef8620b8bae7ed42e3.mockapi.io';
 
 export const getCars = () => axios.get('/advert');
-
-// export const deleteContacts = id => {
-//   return axios.delete(`/contacts/${id}`);
-// };
-
-// export const addContacts = contact => axios.post('/contacts', { ...contact });
